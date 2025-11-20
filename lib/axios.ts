b06041acf40566f15f1a8b567cc7930c7d37e127
Jaseem <mhdjaseemtk@gmail.com>
2025-11-20 20:26:41 +0530

    Set baseURL for axios instance

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: "https://learning-platform-89jxoh0yr-jaseems-projects-fd2a3406.vercel.app/",
 });
 
 export default api;
