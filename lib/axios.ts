4d6b3ddf123c14a52c9e7cab7035e01b17cb8fb4
Jaseem <mhdjaseemtk@gmail.com>
2025-11-20 20:35:53 +0530

    Update axios.ts

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://learning-platform-89jxoh0yr-jaseems-projects-fd2a3406.vercel.app/",
+  baseURL: "https://learning-platform-beryl-iota.vercel.app/",
 });
 
 export default api;
