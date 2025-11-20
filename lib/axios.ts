6d36b09fba7899ffd5df0b82569e002da9e27aaf
Jaseem <mhdjaseemtk@gmail.com>
2025-11-20 20:21:17 +0530

    Change axios base URL to relative path

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,13 +1,10 @@
 import axios from "axios";
 
-const api = axios.create( { baseURL:"https://learning-platform-pe4oh36bp-jaseems-projects-fd2a3406.vercel.app"});
+const api = axios.create({
+  baseURL: "/api",
+});
 
 export default api;
-
-
-
-
- 
 
 
 
