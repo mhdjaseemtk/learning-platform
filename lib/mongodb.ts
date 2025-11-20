dfe3fe114589e1c8b6b0828d2b7d60573125ec68
Jaseem <mhdjaseemtk@gmail.com>
2025-11-20 20:58:25 +0530

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
 
+const MONGODB_URI =
+  "mongodb+srv://jaseemdevxtra_db_user:uZvK0aBdUAu2oc9V@cluster0.ruhxqhc.mongodb.net/learning_platform?retryWrites=true&w=majority";
+
 
 export async function connectDB() {
-if (mongoose.connection.readyState === 1) return;
-await mongoose.connect(process.env.MONGODB_URI!);
-console.log("MongoDB Connected");
+  try {
+    if (mongoose.connection.readyState === 1) {
+      return console.log("MongoDB already connected");
+    }
+
+    await mongoose.connect(MONGODB_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("MongoDB Connection Error:", error);
+    throw new Error("Database connection failed");
+  }
 }
